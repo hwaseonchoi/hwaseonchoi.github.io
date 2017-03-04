8869edc4ebd296e659cfa493530c55630ef5ec0d
novlike <novlike@gmail.com>
2017-03-04 21:31:58 +0100

    MAJ pour exercices atypique

--- footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="footer">
-<h3>Find me</h3>
-<i class="fa fa-code" aria-hidden="true"></i>
-	<a href="http://www.linkedin.com/in/hwa-seon-lecouey"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-	<a href="http://www.instagram.com/novlike/"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-	<a href="mailto:hwaseon.lecouey@gmail.com"><i class="fa fa-envelope" aria-hidden="true"></i></a>
-<i class="fa fa-code" aria-hidden="true"></i>
-</footer>
